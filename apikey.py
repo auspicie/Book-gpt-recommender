4d2487c7b1d5eea9b9cdaf7249b978e69d7f7b5f
SamsudeenBankole <connectsamsudeen@gmail.com>
2025-06-14 18:41:24 +0100

    Initial commit

---
+++ apikey.py
@@ -0,0 +1,68 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "470810f4-b588-4628-9133-964d2387a449",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import os\n",
+    "from dotenv import load_dotenv\n",
+    "\n",
+    "# Load environment variables from .env file\n",
+    "load_dotenv()\n",
+    "\n",
+    "# Get API keys\n",
+    "apikey = os.getenv(\"OPENAI_API\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9060faea-162a-4735-9e1c-083cdaa26013",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "85b01ae3-a6fd-4545-9516-e8e938de6129",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "fb0fc451-e2ef-495f-bab5-082c67196a3d",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
---
+++ app.py
@@ -0,0 +1,73 @@
+import os
+from dotenv import load_dotenv
+
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
+
+# Load .env and API key
+load_dotenv()
+
+# App UI
+st.title('📚🔗 Book GPT Recommender 📚')
+prompt = st.text_input("Type in a book title, you'll get back some good recommendations:")
+
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables=['topic'],
+    template='Give me some book titles about {topic}'
+)
+
+script_template = PromptTemplate(
+    input_variables=['title', 'wikipedia_research'],
+    template='Write me a summary script of the given topic based on this title TITLE: {title} while leveraging this Wikipedia research: {wikipedia_research}'
+)
+
+# Memory
+title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+
+# LLM
+llm = OpenAI(temperature=0.9)
+
+# Chains
+title_chain = LLMChain(llm=llm, prompt=title_template, output_key='title', memory=title_memory, verbose=True)
+script_chain = LLMChain(llm=llm, prompt=script_template, output_key='script', memory=script_memory, verbose=True)
+
+# Wikipedia API
+wiki = WikipediaAPIWrapper()
+
+# Main logic
+if prompt:
+    title = title_chain.run(prompt)
+    # Display clickable book links
+    st.markdown("### 📚 Book Recommendations")
+    for book in title.split('\n'):
+        if book.strip():  # Avoid empty lines
+            search_url = f"https://www.google.com/search?q={book.replace(' ', '+')}+book"
+            st.markdown(f"- [{book}]({search_url})")
+
+    wiki_research = wiki.run(prompt)
+    script = script_chain.run(title=title, wikipedia_research=wiki_research)
+
+    st.subheader("📘 Recommended Book Titles")
+    #st.write(title)
+
+    st.subheader("📄 GPT Summary Script")
+    st.write(script)
+
+    with st.expander('🧠 Title History'):
+        st.info(title_memory.buffer)
+
+    with st.expander('🧠 Script History'):
+        st.info(script_memory.buffer)
+
+    with st.expander('🌐 Wikipedia Research'):
+        st.info(wiki_research)
+st.markdown(
+    '[📘 Learn more about this app](https://github.com/auspicie/POC-Book-GPT-Recommender-with-LangChain) &nbsp;|&nbsp; [🚀 Powered by LangChain](https://www.langchain.com/) &nbsp;|&nbsp; [🔑 Get your OpenAI API key](https://platform.openai.com/account/api-keys)',
+    unsafe_allow_html=True
+)
